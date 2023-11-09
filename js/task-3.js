57388ad4e6cdf740fadf831793d2ca9dc94656bc
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2023-11-09 18:58:59 +0200

    20231109.2

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,7 @@ function getElementWidth(content, padding, border) {
     const contentWidth = Number.parseFloat(content);
     const paddingWidth = Number.parseFloat(padding);
     const borderWidth = Number.parseFloat(border);
+    
     return contentWidth + (paddingWidth * 2) + (borderWidth * 2);
 }
 
